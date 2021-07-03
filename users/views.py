ab5fa3b52351aef6fada667a4d210265a63f8583
vskarasev <karasev997@mail.ru>
2021-07-03 20:30:47 +0300

    добавлена авторизация

--- users/views.py
+++ users/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from users.forms import UserLoginForm
+from django.contrib import auth
+from django.urls import reverse
 
 
 # Create your views here.
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'GeekShop - Авторизация',
+        'form': form,
     }
     return render(request, 'users/login.html', context)
 
