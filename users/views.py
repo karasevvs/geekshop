415d89f2db4493bf235b85a92d4b4425615db9ae
vskarasev <karasev997@mail.ru>
2021-07-08 22:31:55 +0300

    отображение профиля

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -46,5 +46,8 @@ def logout(request):
 
 
 def profile(request):
-    context = {'title': 'GeekShop - Личный кабинет',}
+    form = UserProfileForm(instance=request.user)
+    context = {'title': 'GeekShop - Личный кабинет',
+               'form': form}
     return render(request, 'users/profile.html', context)
+
