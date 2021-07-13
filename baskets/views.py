8a755267721714b41610cd3d7b3546e5b51264fe
vskarasev <karasev997@mail.ru>
2021-07-13 22:11:56 +0300

    прикрутил ajax для корзины

--- baskets/views.py
+++ baskets/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 from products.models import Product
 from baskets.models import Basket
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 
 @login_required
@@ -28,3 +30,18 @@ def basket_remove(request, id):
     basket.delete()
     messages.success(request, 'Товар удален!')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+        baskets = Basket.objects.filter(user=request.user)
+        context = {'baskets': baskets}
+        result = render_to_string('baskets/basket.html', context)
+        return JsonResponse({'result': result})
