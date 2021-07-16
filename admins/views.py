cc5907af889edafd0e5d3a4033ccbb54ced2fc72
vskarasev <karasev997@mail.ru>
2021-07-16 21:08:58 +0300

    создание пользователей

--- admins/views.py
+++ admins/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
+from django.urls import reverse
+
 
 def index(request):
-    return render(request, 'admins/index.html')
+    context = {'title': 'Админ-панель'}
+    return render(request, 'admins/index.html', context)
 
 
 def admin_users(request):
-    context = {'users': User.objects.all()}
+    context = {
+        'title': 'Админ-панель - Пользователи',
+        'users': User.objects.all()}
     return render(request, 'admins/admin-users-read.html', context)
 
 
 def admin_users_create(request):
-    return render(request, 'admins/admin-users-create.html')
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'Админ-панель - Создание пользователя',
+               'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
 
 
 def admin_users_update_delete(request):
