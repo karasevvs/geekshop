057989c07a6917d97a567dcbbb9e6890406b4fd4
vskarasev <karasev997@mail.ru>
2021-07-18 17:42:19 +0300

    добавлено CBV для редактирования юзеров

--- admins/views.py
+++ admins/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import user_passes_test
 
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 
 @user_passes_test(lambda u: u.is_staff)
@@ -28,20 +28,28 @@ class UserCreateView(CreateView):
     success_url = reverse_lazy('admins:admin_users')
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, pk):
-    selected_user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    form = UserAdminProfileForm(instance=selected_user)
-    context = {'title': 'Админ-панель - Редактирование пользователя',
-               'form': form,
-               'selected_user': selected_user,
-               }
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserUpdateView(UpdateView):
+    model = User
+    form_class = UserAdminProfileForm
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admins:admin_users')
+
+
+
+# @user_passes_test(lambda u: u.is_staff)
+# def admin_users_update(request, pk):
+#     selected_user = User.objects.get(id=pk)
+#     if request.method == 'POST':
+#         form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('admins:admin_users'))
+#     form = UserAdminProfileForm(instance=selected_user)
+#     context = {'title': 'Админ-панель - Редактирование пользователя',
+#                'form': form,
+#                'selected_user': selected_user,
+#                }
+#     return render(request, 'admins/admin-users-update-delete.html', context)
 
 
 @user_passes_test(lambda u: u.is_staff)
