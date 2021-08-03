5327fe9718abc9583e42784b808d952c942d474e
vskarasev <karasev997@mail.ru>
2021-08-03 05:30:39 +0300

    подтягивание доп. инфы из вк

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
@@ -70,6 +71,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'products.context_processors.basket',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -155,7 +158,21 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.vk.VKOAuth2',
 )
 
-
 SOCIAL_AUTH_VK_OAUTH2_KEY = os.getenv('SOCIAL_AUTH_VK_OAUTH2_KEY')
 SOCIAL_AUTH_VK_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_VK_OAUTH2_SECRET')
+
+SOCIAL_AUTH_VK_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_VK_OAUTH2_SCOPE = ['email']
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.save_user_profile',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
 
---
+++ users/pipeline.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from django.utils import timezone
+
+import requests
+from social_core.exceptions import AuthForbidden
+
+from users.models import ShopUserProfile
+
+
+def save_user_profile(backend, user, response, *args, **kwargs):
+    if backend.name != 'vk-oauth2':
+        return
+
+    api_url = 'https://api.vk.com/method/users.get'
+    fields_tuple = ('bdate', 'sex', 'about', 'has_photo', 'photo_max_orig')
+    params = {
+        'fields': ','.join(fields_tuple),
+        'access_token': response['access_token'],
+        'v': '5.92',
+    }
+
+    api_response = requests.get(api_url, params=params)
+    if api_response.status_code != 200:
+        return
+
+    data = api_response.json()['response'][0]
+
+    if data.get('sex'):
+        if data['sex'] == 1:
+            user.shopuserprofile.gender = ShopUserProfile.MALE
+        if data['sex'] == 2:
+            user.shopuserprofile.gender = ShopUserProfile.FEMALE
+
+    if data.get('about'):
+        user.shopuserprofile.aboutMe = data['about']
+
+    if data.get('bdate'):
+        bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
+        user.birthday = bdate
+
+        age = timezone.now().date().year - bdate.year
+        if age < 18:
+            user.delete()
+            raise AuthForbidden('social_core.backends.vk.VKOAuth2')
+        user.shopuserprofile.age = age
+    user.save()
+
+
+
+
