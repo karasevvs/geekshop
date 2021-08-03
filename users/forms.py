3804f52f3dd358462374e4151248a95d5571c389
vskarasev <karasev997@mail.ru>
2021-08-03 04:43:46 +0300

    добавлено редактирование профиля пользователя

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
-from users.models import User
+from users.models import User, ShopUserProfile
 import random, hashlib
 
 
@@ -62,3 +62,16 @@ class UserProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'image', 'username', 'email')
+
+
+class ShopUserProfileEdit(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+
+    class Meta:
+        model = ShopUserProfile
+        fields = ('age', 'tagline', 'about_me', 'gender')
